e383c595b620631568fe02f76ccbda97fd4dce6f
Marc-Antoine Marie <mmarie.galaxie@gmail.com>
2025-08-27 17:28:28 +0200

    appsjs useEffect introduction

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import {useState, useEffect} from "react";
 import { FormControl, InputGroup, Container, Button } from "react-bootstrap";
 
 const clientId = import.meta.env.VITE_CLIENT_ID;
@@ -6,6 +7,27 @@ const clientSecret = import.meta.env.VITE_CLIENT_SECRET;
 
 function App() {
 
+const [searchInput, setSearchInput] = useState("");
+const [acessToken, setAccessToken] = useState("");
+
+useEffect (() => {
+  let authParams = {
+    method: "POST",
+    headers: {
+      "Content-type": "application/x-www-form-urlencoded",
+    },
+    body:
+      "grand_type=client_credentials&client_id="+
+      clientId +
+      "&client_secret=" +
+      clientSecret,
+  };
+
+  fetch("hrrps://accounts.spotify.com/api/token", authParams)
+    .then((result) => result.json())
+    
+})
+  
   return (
     <div>
       <Container>
